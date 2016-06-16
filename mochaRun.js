1ac74df6c4f67e238536ee156e490901faae0ee0
Shaban R <srahman@learningobjects.com>
2016-06-16 17:35:49 -0400

    added code for git pull and npm install for all processes

--- mochaRun.js
+++ mochaRun.js
@@ -1,16 +1,129 @@
 var assert = require('chai').assert;
 var cmd = require('node-cmd');
 
-describe('DEAN-Management', function () {
-    describe('Update source', function () {
-        it('check for updates', function () {
-            cmd.run('git pull');
-            assert.equal(-1, [1, 2, 3].indexOf(0));
+var moof = "ded"
+
+
+describe('Update sources', function() {
+
+
+    it('check for updates and install dependencies: dean-integration-tests', function(done) {
+        var output = "data";
+        var output2 = "data2";
+
+        var outputCB = function(data) {
+            output = data;
+        };
+
+        var outputCB2 = function(data) {
+            output2 = data;
+        };
+
+        cmd.run('cd /Users/srahman/Repos/dean-integration-tests/');
+        cmd.get('git pull',function(data){
+            outputCB(data);
+            assert.equal(output.replace(/(\r\n|\n|\r)/gm,"" /*remove newline in output*/), 'Already up-to-date.');
+            done()
+        });
+        cmd.get('npm install',function(data){
+            outputCB2(data);
+            assert.equal(true, true);
         });
     });
-    describe('#indexPof()', function () {
-        it('should poo', function () {
-            assert.equal(-1, -1);
+
+
+
+    it('check for updates and install dependencies: event-aggregator', function(done) {
+        var output = "data";
+        var output2 = "data2";
+
+        var outputCB = function(data) {
+            output = data;
+        };
+
+        var outputCB2 = function(data) {
+            output2 = data;
+        };
+
+        cmd.run('cd /Users/srahman/Repos/event-aggregator/');
+        cmd.get('git pull',function(data){
+            outputCB(data);
+            assert.equal(output.replace(/(\r\n|\n|\r)/gm,"" /*remove newline in output*/), 'Already up-to-date.');
+            done()
+        });
+        cmd.get('npm install',function(data){
+            outputCB2(data);
+            assert.equal(true, true);
+            done()
         });
     });
+
+
+
+    it('check for and install updates: olap-event-consumer', function(done) {
+        var output = "data";
+        var output2 = "data2";
+
+        var outputCB = function(data) {
+            output = data;
+        };
+
+        var outputCB2 = function(data) {
+            output2 = data;
+        };
+
+        cmd.run('cd /Users/srahman/Repos/olap-event-consumer/');
+        cmd.get('git pull',function(data){
+            outputCB(data);
+            assert.equal(output.replace(/(\r\n|\n|\r)/gm,"" /*remove newline in output*/), 'Already up-to-date.');
+            done()
+        });
+        cmd.get('npm install',function(data){
+            outputCB2(data);
+            assert.equal(true, true);
+            done()
+        });
+    });
+
+
+
 });
+
+describe('Start processes', function() {
+
+
+    it('event-aggregator process', function(done) {
+        var output = "data";
+
+        var outputCB = function(data) {
+            output = data;
+        };
+
+        cmd.run('cd /Users/srahman/Repos/event-aggregator/');
+        cmd.get('env DEAN_HOME=/Users/srahman/Repos/ DEAN_ENV=demo node event-aggregator.js',function(data){
+            outputCB(data);
+            done()
+        });
+    });
+
+
+    it('olap-event-consumer process', function(done) {
+        var output = "data";
+
+        var outputCB = function(data) {
+            output = data;
+
+        };
+
+        cmd.run('cd /Users/srahman/Repos/event-aggregator/');
+        cmd.get('env DEAN_HOME=/Users/srahman/Repos/ DEAN_ENV=demo node olap-event-consumer.js',function(data){
+            outputCB(data);
+            done()
+
+        });
+    });
+
+
+
+});
+
